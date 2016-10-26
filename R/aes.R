a8a006ce9a6a3f1e4f7e2a23bef071f5b90c40d9
Michael Lawrence <lawremi@iastate.edu>
2016-10-26 13:39:38 +0000

    rewrite parseArgsForAes(); cleaner, should now catch mapping=NULL
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/biovizBase@123054 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/aes.R
+++ R/aes.R
@@ -1,26 +1,15 @@
 parseArgsForAes <- function(args){
-  ## aesthetics <- plyr:::compact(args[.all_aesthetics])
-  ## aesthetics <- aesthetics[!ggplot2_is.constant(aesthetics)]
-  ## aes_names <- names(aesthetics)
-  ## aesthetics <- rename_aes(aesthetics)
-  ## class(aesthetics) <- "uneval"
-  
-  aes.lst <- unlist(lapply(args, function(x){
-    class(eval(x, parent.frame())) == "uneval" 
-  }))
-  
-  if(length(aes.lst)){
-    idx <- base::which(aes.lst)
-    if(length(idx)){
-      res <- args[[idx]]
-      ## res <- eval(args[[idx]])
-    }else{
-      res <- list()
-    }
-  }else{
-    res <- list()
-  }
-  ## res <- c(res, aesthetics)
+    res <- if ("mapping" %in% names(args)) {
+               args[["mapping"]]
+           } else {
+               Find(function(x) {
+                   class(x) == "uneval"
+               }, args)
+           }
+
+    if (is.null(res))
+        res <- aes()
+    
   idx <- ggplot2_is_calculated_aes(res)
   res[idx] <- ggplot2_strip_dots(res[idx])
   res
